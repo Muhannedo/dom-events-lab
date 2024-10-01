deab28cba92af72f41abe979bd2ed5b7d9161a92
Muhannad <muhanned9980@gmail.com>
2024-10-01 08:40:37 +0300

    finished the lab

--- app.js
+++ app.js
@@ -1,33 +1,75 @@
 /*-------------------------------- Constants --------------------------------*/
-const buttons = document.querySelectorAll(".button");
-const calculator = document.querySelector("#calculator");
-
+const display = document.querySelector(".display");
 /*-------------------------------- Variables --------------------------------*/
+let firstNumber = "";
+let currentOperator = "";
+let secondNumber = "";
 
 /*------------------------ Cached Element References ------------------------*/
-
-/*----------------------------- Event Listeners -----------------------------*/
-buttons.forEach((button) => {
+document.querySelectorAll(".button").forEach((button) => {
   button.addEventListener("click", (event) => {
-    // This log is for testing purposes to verify we're getting the correct value
-    console.log(event.target.innerText);
-    // Future logic to capture the button's value would go here...
+    const buttonClicked = event.target;
+
+    if (buttonClicked.classList.contains("number")) {
+      addNumber(buttonClicked.innerText);
+    } else if (buttonClicked.classList.contains("operator")) {
+      if (buttonClicked.innerText === "C") {
+        resetCalculator();
+      } else {
+        setOperator(buttonClicked.innerText);
+      }
+    } else if (buttonClicked.classList.contains("equals")) {
+      calculateResult();
+    }
   });
 });
-calculator.addEventListener("click", (event) => {
-  // This log is for testing purposes to verify we're getting the correct value
-  // You have to click a button to see this log
-  console.log(event.target.innerText);
-
-  // Example
-  if (event.target.classList.contains("number")) {
-    // Do something with a number
-  }
-
-  // Example
-  if (event.target.innerText === "*") {
-    // Do something with this operator
-  }
-});
-
 /*-------------------------------- Functions --------------------------------*/
+function addNumber(number) {
+  firstNumber += number;
+  updateDisplay(firstNumber);
+}
+// check if the user clicks C or another operator
+function setOperator(operator) {
+  if (firstNumber === "") return;
+  if (secondNumber) calculateResult();
+  currentOperator = operator;
+  secondNumber = firstNumber;
+  firstNumber = "";
+}
+// check if there is any number to do the operation how the calculater will operate
+function calculateResult() {
+  if (secondNumber === "" || firstNumber === "") return;
+  let result;
+
+  const num1 = +secondNumber;
+  const num2 = +firstNumber;
+  if (currentOperator === "+") {
+    result = num1 + num2;
+  } else if (currentOperator === "-") {
+    result = num1 - num2;
+  } else if (currentOperator === "*") {
+    result = num1 * num2;
+  } else if (currentOperator === "/") {
+    result = num1 / num2;
+  }
+
+  firstNumber = result;
+  currentOperator = "";
+  secondNumber = "";
+  updateDisplay(firstNumber);
+}
+// reseting the calculater if the user clicks C
+function resetCalculator() {
+  firstNumber = "";
+  secondNumber = "";
+  currentOperator = "";
+  updateDisplay("");
+  // updateDisplay("0");
+}
+
+function updateDisplay(value) {
+  display.innerText = value;
+}
+// making the display of the calculater 0 at the first beginning
+// updateDisplay("0");
+updateDisplay("");
